53054a3ca6a746dba9ddb31e3015a1856af00acc
Abu Sayeed <abusayeed33343536@gmail.com>
2022-03-19 14:11:46 +0600

    first commit

---
+++ resources/views/pages/Admin/index.blade.php
@@ -0,0 +1,69 @@
+@extends('layouts.Admin.master')
+
+
+@section('admincontent')
+
+                <!-- Dashboard Ecommerce Starts -->
+                <section id="card-style-variation">
+                    <h5 class="mt-3 mb-2"></h5>
+                    <!-- Solid -->
+                    <div class="row">
+                        <div class="col-md-6 col-xl-4">
+                            <div class="card bg-primary text-white">
+                                <div class="card-body">
+                                    <h3 class="card-title text-white">Total Balance</h3>
+                                    <h1 class="card-text">500$</h1>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-xl-4">
+                            <div class="card bg-secondary text-white">
+                                <div class="card-body">
+                                    <h3 class="card-title text-white">Total Sale</h3>
+                                    <h1 class="card-text">1000$</h1>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-xl-4">
+                            <div class="card bg-success text-white">
+                                <div class="card-body">
+                                    <h3 class="card-title text-white">Total Countries</h3>
+                                    <h1 class="card-text">108</h1>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-xl-4">
+                            <div class="card bg-danger text-white">
+                                <div class="card-body">
+                                    <h3 class="card-title text-white">Total Applications</h3>
+                                    <h1 class="card-text">500</h1>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-xl-4">
+                            <div class="card bg-warning text-white">
+                                <div class="card-body">
+                                    <h3 class="card-title text-white">Total Users</h3>
+                                    <h1 class="card-text">300</h1>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-xl-4">
+                            <div class="card bg-info text-white">
+                                <div class="card-body">
+                                    <h3 class="card-title text-white">Total Deposits</h3>
+                                    <h1 class="card-text">$5000</h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Outline -->
+
+                </section>
+                <!-- Dashboard Ecommerce ends -->
+
+
+    <!-- END: Content-->
+
+@endsection
